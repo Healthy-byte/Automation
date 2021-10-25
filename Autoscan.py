7db540fe2e58bf14c2786b0e72faadc98960d8ff
Healthy-byte <73305915+Healthy-byte@users.noreply.github.com>
2021-10-25 15:57:40 -0400

    Add files via upload

--- Autoscan.py
+++ Autoscan.py
@@ -2,17 +2,36 @@ import Scan_methods
 import art
 import socket
 import time
+import sys
+import yaml
+
 
 def main():
     art.tprint("AutoScan", font="random")
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    if local_ip != "127.0.0.1":
-        print (f"My ip address is {local_ip}")
+    system = sys.platform
+    if system == "win32":
+        my_ip = socket.gethostbyname(socket.gethostname())
+        print(my_ip)
+    else:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+            my_ip =  s.getsockname()[0] # Giver både IP og port. Skal bruge indeks 0
+            #print (my_ip)
+        except:
+            print("Not able to connect to the internet, waiting 30 seconds and trying again")
+            time.sleep(30)
+            main()
+
+    #hostname = socket.gethostname()
+    #my_ip = socket.gethostbyname(hostname)
+
+    if my_ip != "127.0.0.1":
+        print (f"My ip address is {my_ip}")
         #Scanning live hosts on network
-        host_list = Scan_methods.hostthreading(str(local_ip))
+        host_list = Scan_methods.hostthreading(str(my_ip))
         print("SCANNING NETWORK...\n")
-        if list =="":
+        if host_list =="":
             print("NO HOSTS FOUND")
         else:
             #print(host_list)
@@ -26,7 +45,8 @@ def main():
         for index in host_list:
             print (f"Scanning IP: {index}\n")
             open_ports.append(Scan_methods.port_scanner(index))
-            nice_print = open_ports.append(Scan_methods.port_scanner(index))
+            
+            #nice_print = open_ports.append(Scan_methods.port_scanner(index))
             #pprint.pprint(nice_print)
 
 
@@ -44,12 +64,11 @@ def main():
  \_X |_| _|_ \_ |\ __) \_ /--\ | \| 
                                     ''')
 
-        print("\n Doing extensive scanning of open ports using NMAP: \n")
-        
         Scan_methods.service_on_port()
+        with open("output_file.yml", "w") as outfile:
+            yaml.dump(Scan_methods.service_on_port(), outfile, default_flow_style=False)
         #for ip_adresses in host_list:
             #Scan_methods.service_on_port(ip_adresses)
-        
     else:
         print("Not connected to the internet, waiting 1 minute and trying again")
         time.sleep(60)
--- Scan_methods.py
+++ Scan_methods.py
@@ -1,11 +1,10 @@
 import subprocess
 import os
-import threading 
+import threading
 import sys
 import socket
 import nmap
 import yaml
-
 
 host_list = []
 #host_list_print = ""
@@ -14,13 +13,14 @@ def hostscanner(IP):
     global host_list_print
     system = sys.platform
     DEVNULL = open(os.devnull, "w") #Skraldespand vaiabel
-    
     if system == 'win32':
-        cmd_kommando = "ping -n 1 -w 20 " + IP # -n er antal gange -w er ms
+        svar = subprocess.run(["ping", "-n", "1", "-w", "20", IP], stdout=DEVNULL)
+        #cmd_kommando = f"'ping', '-n', '1', '-w', '20', '{IP}'" # -n er antal gange -w er ms
     else:
-        cmd_kommando = "ping -c 1 " + IP
-    svar = subprocess.call(cmd_kommando, stdout=DEVNULL)
-    if svar == 0:
+        svar = subprocess.run(["ping", "-c", "1", IP], stdout=DEVNULL)
+        #cmd_kommando = f'"ping", "-c", "1", "{IP}"'
+    #svar = subprocess.run([cmd_kommando], stdout=DEVNULL) #Dette er blevet lavet om til nyere metode af subprocess modulet
+    if svar.returncode == 0:
         host_list.append(IP)
         #host_list_print += "HOST: " + IP + " IS LIVE!\n"      
 
@@ -69,22 +69,23 @@ def tcp_connnecter(ip_input, port_number):
         print("Error occured")
     #return port_list, "SHIT AINT WORKING"
 
+
 def port_scanner(ip_input):
     global port_list_til_print
     global raw_ports
     threads = [] # tom liste
-    for ports in range(10000):
+    for ports in range(100):
         t = threading.Thread(target=tcp_connnecter, args=(ip_input, ports))
         threads.append(t) 
         #Her tilføjer alle de tråde der skal startes til ip'en
         #Dette bliver tilføjet til vores threads liste som vi kører alle sammen på samme tid senere hen
     
-    for i in range(10000):
+    for i in range(100):
         threads[i].start()
         #Her starter vi alle de threads vi har gjort klart før
         #Disse threads bliver kaldt ved hjælp af indekseringen [i]
 
-    for i in range(10000):
+    for i in range(100):
         threads[i].join()
     return port_list_til_print
 
@@ -102,19 +103,14 @@ def service_on_port():
     #Bliver nok nød til at læse lidt igennem source for at fatte hvordan ting bliver printet pænt
     # https://bitbucket.org/xael/python-nmap/src/master/nmap/nmap.py
     for i, j in ip_and_port_for_scan:
-        print(f"Service / Script Scanning {i} : {j}")
+        print(f"Service / Scanning {i} : {j}")
         nmscan.scan(i, str(j), "-sC -sV") #Den rigtige variabel (tager meget lang tid)
-        print(nmscan.command_line())
-        print(nmscan.csv())
         var_for_print = nmscan.analyse_nmap_xml_scan()
-        print (yaml.dump(var_for_print, default_flow_style=False))
-        #dom = xml.dom.minidom.parseString(var_for_print)
-        #print(dom)
-        
-        #for i, j in var_for_print.items:
-         #   print (f"Dette er en test: {i}")
-          #  for key in j:
-           #     print(key + " : " , j[key])
+        print(yaml.dump(var_for_print, default_flow_style=False))
+
+        #print(nmscan.command_line())
+        #print(nmscan.csv())
+        #print(nmscan.analyse_nmap_xml_scan())
         #pprint.pprint(nmscan.analyse_nmap_xml_scan)
         #print(nmscan.scaninfo())
         #print(nmscan.get_nmap_last_output())
