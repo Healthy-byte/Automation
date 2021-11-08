83cc2360cf9b014f1aa30f61a4a495e6e0f84fae
Healthy-byte <73305915+Healthy-byte@users.noreply.github.com>
2021-11-08 07:29:35 -0500

    Add files via upload

--- Autoscan.py
+++ Autoscan.py
@@ -16,59 +16,29 @@ def main():
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             s.connect(("8.8.8.8", 80))
-            my_ip =  s.getsockname()[0] # Giver både IP og port. Skal bruge indeks 0
-            #print (my_ip)
+            my_ip = s.getsockname()[0]
+# Giver både IP og port. Skal bruge indeks 0
+
         except:
-            print("Not able to connect to the internet, waiting 30 seconds and trying again")
-            time.sleep(30)
+            print("Not able to connect to the internet, waiting 60 seconds and trying again")
+            time.sleep(60)
             main()
 
-    #hostname = socket.gethostname()
-    #my_ip = socket.gethostbyname(hostname)
-
-    if my_ip != "127.0.0.1":
-        print (f"My ip address is {my_ip}")
-        #Scanning live hosts on network
+    if my_ip != "127.0.0.1" or "127.0.1.1":
+        print(f"My ip address is {my_ip}")
         host_list = Scan_methods.hostthreading(str(my_ip))
+# Scanning live hosts on network
         print("SCANNING NETWORK...\n")
-        if host_list =="":
+
+        if host_list == "":
             print("NO HOSTS FOUND")
         else:
-            #print(host_list)
             for i in host_list:
                 print(f"HOST: ----> {i} <---- IS LIVE", end="\n")
-        print ("\n")
-        #out_file = open("Test_Output.txt", "w")
-        open_ports = []
-        list_til_scan = {}
-        #print (host_list)
-        for index in host_list:
-            print (f"Scanning IP: {index}\n")
-            open_ports.append(Scan_methods.port_scanner(index))
-            
-            #nice_print = open_ports.append(Scan_methods.port_scanner(index))
-            #pprint.pprint(nice_print)
-
-
-        open_ports = open_ports[0]
-        print('''
-  _      ___  _     __  _           
- / \ | |  |  /  |/ (_  /   /\  |\ | 
- \_X |_| _|_ \_ |\ __) \_ /--\ | \| 
-                                    ''')
-        for i in open_ports:
-            print (i)
-        print('''
-  _      ___  _     __  _           
- / \ | |  |  /  |/ (_  /   /\  |\ | 
- \_X |_| _|_ \_ |\ __) \_ /--\ | \| 
-                                    ''')
-
-        Scan_methods.service_on_port()
-        with open("output_file.yml", "w") as outfile:
-            yaml.dump(Scan_methods.service_on_port(), outfile, default_flow_style=False)
-        #for ip_adresses in host_list:
-            #Scan_methods.service_on_port(ip_adresses)
+                print (f"Scanning: {i}...", end="\n")
+                open_ports = Scan_methods.port_scanner(i)
+        Scan_methods.service_on_port() 
+# Gem metoden i en variabel og smid den i fil til print som senere kan sendes
     else:
         print("Not connected to the internet, waiting 1 minute and trying again")
         time.sleep(60)
